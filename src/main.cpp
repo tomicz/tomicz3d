30e9ebcfefa3e9f089aca13f5a723a29a0d09964
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2025-01-05 18:21:15 +0100

    Created OpenGL Window

--- src/main.cpp
+++ src/main.cpp
@@ -2,136 +2,28 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-void check_shader_compile_errors(unsigned int shader) {
-    int success;
-    char info_log[512];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(shader, 512, NULL, info_log);
-        std::cerr << "Shader compilation failed:\n" << info_log << std::endl;
-        glfwTerminate();
-        exit(-1);
-    }
-}
-
 int main() {
-    GLFWwindow* window;
-
-    if (!glfwInit()) {
-        return -1;
-    }
-
-    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+    glfwInit();
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    window = glfwCreateWindow(mode->width, mode->height, "tomicz3d - Game Engine", NULL, NULL);
-    if (!window) {
+    GLFWwindow* window = glfwCreateWindow(800, 600, "tomiczrd", NULL, NULL);
+
+    if(window == NULL){
+        std::cout << "Failed to create GLFW Window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
-
-    GLenum err = glewInit();
-    if (err != GLEW_OK) {
-        std::cerr << "GLEW initialization failed: " << glewGetErrorString(err) << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    float scale = 1.0f;  
     
-    float vertices[] = {
-         0.5f * scale,  0.5f * scale, 0.0f,  
-         0.5f * scale, -0.5f * scale, 0.0f,  
-        -0.5f * scale, -0.5f * scale, 0.0f,  
-        -0.5f * scale, -0.5f * scale, 0.0f,  
-        -0.5f * scale,  0.5f * scale, 0.0f,  
-         0.5f * scale,  0.5f * scale, 0.0f   
-    };
-
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    const char* vertexShaderSource = 
-        "#version 410 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "uniform float scale;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x * scale, aPos.y * scale, aPos.z, 1.0);\n"
-        "}\0";
-    
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    check_shader_compile_errors(vertexShader);
-
-    const char* fragmentShaderSource = 
-        "#version 410 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
-        "}\0";
-
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    check_shader_compile_errors(fragmentShader);
-
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    int success;
-    char info_log[512];
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, info_log);
-        std::cerr << "Shader program linking failed:\n" << info_log << std::endl;
-        return -1;
-    }
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    int scaleLocation = glGetUniformLocation(shaderProgram, "scale");
-    
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-        
-        float scale = 0.3f;  
-        glUniform1f(scaleLocation, scale);
-        
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-
-        glfwSwapBuffers(window);
+    while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
