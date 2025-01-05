1c93c7fe6e500023b1285e73b1bb6c9dba44b66e
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2025-01-05 18:52:51 +0100

    Added window background color

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
 #include <GLFW/glfw3.h>
 
 int main() {
-    glfwInit();
+    if(!glfwInit()){
+        std::cout << "Couln't intilise GLFW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
@@ -18,6 +22,11 @@ int main() {
     }
 
     glfwMakeContextCurrent(window);
+
+    glViewport(0, 0, 800, 600);
+    glClearColor(.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
     
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
